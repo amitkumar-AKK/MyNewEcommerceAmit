2668eee7db09f641bab3f66ffdd44fe88e3b12ba
amitkumarisdevelopeer <ak14prakas@gmail.com>
2025-07-16 05:03:59 +0530

    addded contact and email address in footer list

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,7 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+           Amit has designed a dynamic e-commerce website that allows users to browse various products and make purchases online easily.
           </p>
         </div>
 
@@ -40,8 +37,8 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+91 7319865270</p>
+              <p>amitkumar77326@gmail.com</p>
             </div>
           </div>
         </div>
